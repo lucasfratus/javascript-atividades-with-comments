f1681d4a36933f7547626c8f7f6dca71314bd0b8
Victor Costa <vg47683@gmail.com>
2025-07-04 11:35:38 -0300

    refatoração dos exercicios

--- todo-list/script.js
+++ todo-list/script.js
@@ -2,24 +2,49 @@ const input = document.getElementById('input-tarefa');
 const botaoAdicionar = document.getElementById('adicionar');
 const listaTarefas = document.getElementById('lista-tarefas');
 
-// adicionar tarefa na lista
 botaoAdicionar.addEventListener('click', () => {
   const tarefa = input.value.trim();
   if (tarefa) { // se o input nao for vazio, adiciona na lista
     const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = tarefa;
+    
+    const textoTarefa = document.createElement('span'); // cria o span da tarefa
+    textoTarefa.textContent = tarefa;
 
-    // adicionar opcao de remover tarefa
-    const remover = document.createElement('span');
-    remover.textContent = 'X';
-    remover.classList.add('remover');
+    const botoesContainer = document.createElement('div'); // cria a div que vai conter os botoes de editar/salvar e remover
+    botoesContainer.className = 'botoes-container';
+
+    // botoes de editar e salvar
+    const editar = document.createElement('button');
+    editar.textContent = 'Editar';
+    editar.className = 'editar';
+
+    editar.addEventListener('click', () => {
+      if (editar.textContent === 'Editar') {
+        textoTarefa.contentEditable = true; // forna o texto editavel
+        textoTarefa.focus(); // foca no texto para o usuário já poder digitar
+        editar.textContent = 'Salvar'; // muda o texto do botão para "Salvar"
+        editar.className = 'salvar'; // muda a classe para .salvar 
+      } else {
+        textoTarefa.contentEditable = false; // trava a edição do texto
+        editar.textContent = 'Editar'; // volta o texto do botão para "Editar"
+        editar.className = 'editar'; // volta a classe .editar (azul)
+      }
+    });
+
+    // botao remover
+    const remover = document.createElement('button');
+    remover.textContent = 'Deletar';
+    remover.className = 'remover';
     remover.addEventListener('click', () => {
       listaTarefas.removeChild(li);
     });
 
-    li.append(span);
-    li.appendChild(remover);
+    botoesContainer.appendChild(editar);
+    botoesContainer.appendChild(remover);
+
+    li.appendChild(textoTarefa);
+    li.appendChild(botoesContainer);
+
     listaTarefas.appendChild(li);
     input.value = '';
   }
